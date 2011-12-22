85044d49575fa9b49dcde93601bdedc5dcf5a563
jakub.vrana <jakub.vrana@9d62e163-8451-0410-9e3b-c7d3cf8872d9>
2011-12-22 06:40:15 +0000

    Dual licensed (fixes issue #6)

--- php-initialized.inc.php
+++ php-initialized.inc.php
@@ -13,6 +13,7 @@
 * @author Jakub Vrana, http://www.vrana.cz/
 * @copyright 2008 Jakub Vrana
 * @license http://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0
+* @license http://www.gnu.org/licenses/gpl-2.0.html GNU General Public License, version 2 (one or other)
 * @version $Date::                           $
 */
 function check_variables($filename, $initialized = array(), $function = "", $class = "", $in_string = false, $tokens = null, $i = 0, $single_command = null) {
